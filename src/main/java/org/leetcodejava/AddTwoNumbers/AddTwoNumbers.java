44a4277f9bac576fa84b564c7e4c659b20d21baf
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2025-12-18 23:49:05 +0700

    Add addTwoNumbers exercise

---
+++ src/main/java/org/leetcodejava/AddTwoNumbers/AddTwoNumbers.java
@@ -0,0 +1,13 @@
+package org.leetcodejava.AddTwoNumbers;
+
+public class AddTwoNumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode result = dummyHead;
+
+        while (l1 != null || l2 != null) {
+
+            int sum = 0;
+        }
+    }
+}
