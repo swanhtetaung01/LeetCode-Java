bea4262857645fa0fcbf6a14954e9078715afef7
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2025-12-19 23:19:31 +0700

    Add LongestSubstringWithoutRepeatingCharacters exercise

---
+++ src/main/java/org/leetcodejava/LongestSubstringNoRepeatedChar/Solution.java
@@ -0,0 +1,12 @@
+package org.leetcodejava.LongestSubstringNoRepeatedChar;
+
+public class Solution {
+    String s;
+    public Solution(String s){
+
+    }
+    public int lengthOfLongestSubstring(String s) {
+
+
+    }
+}
