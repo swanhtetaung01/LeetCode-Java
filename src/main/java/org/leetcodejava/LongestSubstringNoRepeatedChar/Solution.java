eeaafc49d2ffe8e3ae95743218e6ef9c155e475e
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2025-12-19 23:30:43 +0700

    Update on LongestSubstring exercise

--- src/main/java/org/leetcodejava/LongestSubstringNoRepeatedChar/Solution.java
+++ src/main/java/org/leetcodejava/LongestSubstringNoRepeatedChar/Solution.java
@@ -3,10 +3,17 @@ package org.leetcodejava.LongestSubstringNoRepeatedChar;
 public class Solution {
     String s;
     public Solution(String s){
-
+        this.s = s;
     }
     public int lengthOfLongestSubstring(String s) {
+        int length = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(0) != s.charAt(1)){
+                length++;
+                while (i < s.length()){
 
-
+                }
+            }
+        }
     }
 }
