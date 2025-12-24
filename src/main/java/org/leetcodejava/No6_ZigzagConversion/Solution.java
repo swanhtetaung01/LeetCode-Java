a383d206baf6eba900b68f65cfa10ec683e8b4f7
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2025-12-24 23:53:01 +0700

    Update zigzag conversion

--- src/main/java/org/leetcodejava/No6_ZigzagConversion/Solution.java
+++ src/main/java/org/leetcodejava/No6_ZigzagConversion/Solution.java
@@ -2,7 +2,19 @@ package org.leetcodejava.No6_ZigzagConversion;
 
 public class Solution {
     public String convert(String s, int numRows) {
+        if (numRows == 1 || s.length() <= numRows) return s;
 
+        StringBuilder[] row = new StringBuilder[numRows];
+        for (int i = 0; i < numRows; i++)
+            row[i] = new StringBuilder();
+
+        int curRow = 0;
+        boolean goingDown = false;
+
+        for (char c : s.toCharArray()) {
+            row[curRow].append(c);
+            if (curRow == 0 || curRow == numRows - 1) goingDown = !goingDown;
+        }
 
         return;
     }
