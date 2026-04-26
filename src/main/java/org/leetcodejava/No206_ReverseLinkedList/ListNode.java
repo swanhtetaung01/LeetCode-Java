c17c909642071c67179a03d24eae0a448e00c8ce
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2026-04-26 23:42:38 +0700

    Add custom problem

---
+++ src/main/java/org/leetcodejava/No206_ReverseLinkedList/ListNode.java
@@ -0,0 +1,10 @@
+package org.leetcodejava.No206_ReverseLinkedList;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
---
+++ src/main/java/org/leetcodejava/No206_ReverseLinkedList/reverseLinkedList.java
@@ -0,0 +1,7 @@
+package org.leetcodejava.No206_ReverseLinkedList;
+
+public class reverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        
+    }
+}
